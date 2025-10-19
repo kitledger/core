c7129adcd114970750ab33e6bd75cfbec68b24dd
Alejandro Barrera Aponte <alebarrera76@gmail.com>
2025-10-19 15:39:40 -0300

    WIP

--- src/main.ts
+++ src/main.ts
@@ -31,41 +31,6 @@ console.log("--- Script Execution Finished ---");
 console.log("Final Result:", result);
 console.log("---------------------------------");
 
-/**
- * Sample query execution to demonstrate the executeQuery function.
- */
-const queryParams: Query = {
-    select: [
-        { column: "accounts.id", as: "account_id" },
-        { column: "parent.name", as: "parent_name" },
-    ],
-    joins: [
-        {
-            type: "left",
-            table: "accounts",
-            as: "parent",
-            onLeft: "accounts.parent_id",
-            onRight: "parent.id",
-        }
-    ],
-    where: [
-        {
-            connector: "and",
-            conditions: [
-                // This condition correctly finds all accounts that have a parent
-                { column: "accounts.parent_id", operator: "not_empty", value: true },
-            ],
-        }
-    ],
-    orderBy: [
-        { column: "accounts.created_at", direction: "desc" },
-    ],
-};
-
-const queryResult = await executeQuery(accounts, queryParams);
-
-console.log("--- Executing Sample Query ---", queryResult);
-
 // --- Server and CLI Startup Logic ---
 
 const args = Deno.args;
