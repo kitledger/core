681c7c9cff001ce7d9487e01feec02b49d656b4c
Alejandro Barrera Aponte <alebarrera76@gmail.com>
2025-10-24 09:31:45 -0300

    Scaffold frontend dependencies

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,34 @@
 import "./style.css";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen.ts";
+
+// Create a client for React Query
+const queryClient = new QueryClient();
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
+		</StrictMode>,
+	);
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite'
 import deno from '@deno/vite-plugin'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), react(), tailwindcss()],
+  plugins: [
+	deno(),
+	tanstackRouter({
+		target: 'react',
+      	autoCodeSplitting: true,
+		routesDirectory: './src/client/routes',
+		generatedRouteTree: 'routeTree.gen.ts',
+	}),
+	react(),
+	tailwindcss()
+],
   root: 'src/client',
   build: {
 	assetsDir: "assets",
