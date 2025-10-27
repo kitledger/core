92751ff5d4d979934b50704852367fd9a1ae0311
Alejandro Barrera Aponte <alebarrera76@gmail.com>
2025-10-27 00:49:36 -0300

    App Shell Layouts

--- client/routes/$app.tsx
+++ client/routes/$app.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { AppShell, Burger, Group, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 export const Route = createFileRoute("/$app")({
 	component: AppLayout,
@@ -9,7 +11,27 @@ export const Route = createFileRoute("/$app")({
  */
 export function AppLayout() {
 
+	const [opened, { toggle }] = useDisclosure();
+
 	return (
-		<Outlet />
+		<AppShell
+		header={{ height: 60 }}
+		navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+		padding="md"
+		>
+		<AppShell.Header>
+			<Group h="100%" px="md">
+			<Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+			Header has a burger icon below sm breakpoint
+			</Group>
+		</AppShell.Header>
+		<AppShell.Navbar p="md">
+			Navbar is collapsed on mobile at sm breakpoint. At that point it is no longer offset by
+			padding in the main element and it takes the full width of the screen when opened.
+		</AppShell.Navbar>
+		<AppShell.Main>
+			<Outlet />
+		</AppShell.Main>
+		</AppShell>
 	);
 }
--- client/routes/index.tsx
+++ client/routes/index.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Group, Burger } from "@mantine/core";
 
 export const Route = createFileRoute("/")({
 	component: Index,
@@ -10,15 +12,29 @@ export const Route = createFileRoute("/")({
 });
 
 export function Index() {
+	const [opened, { toggle }] = useDisclosure();
 	return (
-		<div>
-			<Link
-				to="/$app/dashboard"
-				params={{ app: "main" }}
-			>
-				App
-			</Link>
-			<Link to="/accounts/login">Login</Link>
-		</div>
+		<AppShell
+		header={{ height: 60 }}
+		padding="md"
+		>
+		<AppShell.Header>
+			<Group h="100%" px="md">
+			<Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+				Header has a burger icon below sm breakpoint
+			</Group>
+		</AppShell.Header>
+		<AppShell.Main>
+			<div>
+				<Link
+					to="/$app/dashboard"
+					params={{ app: "main" }}
+				>
+					App
+				</Link>
+				<Link to="/accounts/login">Login</Link>
+			</div>
+		</AppShell.Main>
+		</AppShell>
 	);
 }
